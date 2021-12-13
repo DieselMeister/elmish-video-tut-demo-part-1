50bee8d5abb99525413d013b9a0e1fc3e7cef35a
Daniel Hardt <info@hardt-solutions.de>
2021-12-13 14:32:55 +0100

    Add project files.

---
+++ src/Main.fs
@@ -0,0 +1,60 @@
+﻿module Main
+
+
+open Elmish
+open Lit
+open Lit.Elmish
+
+
+(*
+         _______                        _______
+        |       |                      |       |     
+        | Model |--------render------->| View  |                 
+        |_______|                      |_______|                         
+            A                              |                             
+            |                              |                     
+            |                              |                     
+            |                              |                     
+            |                              |                     
+            |                              |                     
+            |                           ___V____
+            |                          |        |                                
+            ---------------------------| Update |                                
+                                       |________|
+
+ _______________________
+|  our little Todo app  |
+|                       |
+|  * Todo 1  [Remove]   |
+|  * Todo 2  [Remove]   |
+|                       |
+|  [_input_] [Add]      |
+|_______________________|
+*)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Program.mkProgram init update view
+|> Program.withLit "elmish-lit"
+|> Program.run
